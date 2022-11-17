4a9c0c9cae48d290a0ac25c3df4ba1f6714a6604
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-11-17 09:52:43 +0100

    save

---
+++ Conditionals/script.js
@@ -0,0 +1,21 @@
+
+function MissedGoal() {
+    return <h1>MISSED!</h1>;
+}
+
+function MadeGoal() {
+    return <h1>Goal!</h1>;
+}
+
+
+function Goal(props) {
+    const isGoal = props.isGoal;
+
+    if (isGoal) {
+        return <MadeGoal />;
+    }
+    return <MissedGoal />;
+}
+
+ReactDOM.render(<Goal isGoal={1} />,document.getElementById('root'));
+ReactDOM.render(<Goal isGoal={false} />,document.getElementById('root1'));
--- Events/script.js
+++ Events/script.js
@@ -1,11 +1,24 @@
 function Football() {
-    const shoot = (a) => {
-      alert(a);
-    }
-  
-    return (
-      <button onClick={() => shoot("Goaaaaaaal!")}>shoot!</button>
-    );
+  const shoot = () => {
+    alert("Great Shot!");
   }
+
+  return (
+    <button onClick={shoot}>Click and shot!</button>
+  );
+}
+
+ReactDOM.render(<Football />,document.getElementById('root'));
+
+
+// function Football() {
+//     const shoot = (a) => {
+//       alert(a);
+//     }
   
-ReactDOM.render(<Football />,document.getElementById('root'))
+//     return (
+//       <button onClick={() => shoot("Goaaaaaaal!")}>shoot!</button>
+//     );
+//   }
+  
+// ReactDOM.render(<Football />,document.getElementById('root'));
