f56e459757300f644397fa80ff786b30494ec399
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-11-17 10:18:00 +0100

    save

--- Events/script.js
+++ Events/script.js
@@ -1,24 +1,39 @@
-function Football() {
-  const shoot = () => {
-    alert("Great Shot!");
-  }
+// function Football() {
+//   const shoot = () => {
+//     alert("Great Shot!");
+//   }
 
-  return (
-    <button onClick={shoot}>Click and shot!</button>
-  );
-}
-
-ReactDOM.render(<Football />,document.getElementById('root'));
+//   return (
+//     <button onClick={shoot}>Click and shot!</button>
+//   );
+// }
+// ReactDOM.render(<Football />,document.getElementById('root'));
 
 
 // function Football() {
-//     const shoot = (a) => {
-//       alert(a);
-//     }
-  
-//     return (
-//       <button onClick={() => shoot("Goaaaaaaal!")}>shoot!</button>
+//       const shoot = (a) => {
+//           alert(a);
+//         }
+    
+//         return (
+//             <button onClick={() => shoot("Goaaaaaaal!")}>shoot!</button>
 //     );
 //   }
   
 // ReactDOM.render(<Football />,document.getElementById('root'));
+
+
+
+function ActionLink() {
+  function handleClick(a) {
+    a.preventDefault();
+    console.log('Le lien a été cliqué.');
+  }
+
+  return (
+    <a href="#" onClick={handleClick}>
+      Clique ici
+    </a>
+  );
+}
+ReactDOM.render(<ActionLink />,document.getElementById('root'));
